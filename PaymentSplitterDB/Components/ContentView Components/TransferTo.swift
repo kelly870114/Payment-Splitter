35f76217f63a91227f3a3312e33fa94368f54894
Ginny Huang <kelly87011431@gmail.com>
2022-11-17 00:55:56 -0800

    Create Crypto Currency Picker

---
+++ PaymentSplitterDB/Components/ContentView Components/TransferTo.swift
@@ -0,0 +1,45 @@
+//
+//  TransferTo.swift
+//  PaymentSplitterDB
+//
+//  Created by 黃嬿羽 on 2022/11/16.
+//
+
+import SwiftUI
+
+struct TransferTo: View {
+    var body: some View {
+        var FontMini : Font = Font.custom("Nunito", size: 13)
+        var FontSmall : Font = Font.custom("Nunito", size: 16)
+        var FontRegular : Font = Font.custom("Nunito", size: 20)
+        var FontLarge : Font = Font.custom("Nunito", size: 30)
+        
+        VStack{
+            Text("Transfer to")
+                .font(FontRegular)
+                .foregroundColor(Color.white)
+                .fontWeight(.bold)
+            Image("Amy")
+                .resizable()
+                .frame(width: 45, height: 45)
+                .clipShape(Circle())
+            Text("Amy Ho")
+                .font(FontSmall)
+                .foregroundColor(Color.white)
+                .fontWeight(.bold)
+            
+            CryptoCurrPicker()
+        }
+        .padding()
+        .background(Color("Card"))
+        .cornerRadius(20)
+        
+        
+    }
+}
+
+struct TransferTo_Previews: PreviewProvider {
+    static var previews: some View {
+        TransferTo()
+    }
+}
--- PaymentSplitterDB/CreatePaymentStep2View.swift
+++ PaymentSplitterDB/CreatePaymentStep2View.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct CreatePaymentStep2View: View {
+    init() {
+        //Use this if NavigationBarTitle is with displayMode = .inline
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white,.font : UIFont(name: "Nunito-Bold", size: 30)!]
+    }
+    var FontMini : Font = Font.custom("Nunito", size: 13)
+    var FontSmall : Font = Font.custom("Nunito", size: 16)
+    var FontRegular : Font = Font.custom("Nunito", size: 20)
+    var FontLarge : Font = Font.custom("Nunito", size: 30)
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            GeometryReader{ geometry in
+                VStack{
+                    ZStack(alignment: .topLeading){
+                        RoundedRectMid()
+                            .frame(width: geometry.size.width, height: geometry.size.height * 0.9).ignoresSafeArea()
+                        VStack(alignment: .center, spacing: 20){
+                            
+                            Image("Step1")
+                                .resizable()
+                                .frame(width:150, height: 25)
+                                .padding(.top, 50)
+                            TotalBalance()
+                            
+                        }
+                    }
+                    .frame(width: geometry.size.width, height: geometry.size.height*0.8 )
+                    .background(Color.pink)
+                    VStack(alignment: .leading){
+                        Text("HI")
+                        
+                            
+                    }.background(Color.green)
+                    
+                }
+            }
+            .navigationBarTitle(Text("Payment").font(FontLarge), displayMode: .inline)
+        }
     }
 }
 
